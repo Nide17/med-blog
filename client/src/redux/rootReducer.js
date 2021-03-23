13559f5490ebda2eb18e67cf1dc1dbe94626fe4e
Nide17 <nidedrogba@gmail.com>
2021-03-23 22:19:51 +0200

    backend auth

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import postsReducer from './posts/posts.reducer';
 import questionsReducer from './questions/questions.reducer';
+import errorReducer from './error/error.reducer';
+import authReducer from './auth/auth.reducer';
 
-const rootReducer = combineReducers({ postsReducer, questionsReducer });
+const rootReducer = combineReducers({ postsReducer, questionsReducer, errorReducer, authReducer });
 
 export default rootReducer;
