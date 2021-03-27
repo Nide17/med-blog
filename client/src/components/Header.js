4ad9bd98fa195a2f41367b9a999c7d9b5f550e25
Nide17 <nidedrogba@gmail.com>
2021-03-27 14:37:21 +0200

    responsive header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = (props) => {
         <>
             <NavbarText className="mx-0 text-warning">
                 <span>
-                    <small>{user ? `Welcome ${user.name}` : ''}</small>
+                    <small>{user ? `Welcome ${user.name.split(" ").splice(-1)}` : ''}</small>
                 </span>
             </NavbarText>
 
@@ -45,14 +45,14 @@ const Header = (props) => {
 
     const blinkBtn = (location.pathname === '/questions' || location.pathname === '/create') ? '' :
         <>
-            <Link to="/questions" className="text-white blink_me mt-1 mr-2">Quiz</Link>
+            <Link to="/questions" className="text-white blink_me mt-1 mx-1">Quiz</Link>
             {isAuthenticated ? <Link to="/create" className="text-warning ml-2 mt-1">Create Quiz</Link> : null}
         </>
 
     return (
         <header style={{ boxShadow: "0 2px 10px -1px rgba(0,0,0,0.75)" }}>
 
-            <Navbar color="primary" light expand="md" className="px-md-5 py-md-3">
+            <Navbar color="primary" light expand="md" className="px-md-1 px-lg-5 py-md-3">
                 <NavbarBrand href="/" className="text-white" style={{ fontWeight: "900" }}>
                     <img src={logo} alt="logo" />
                 </NavbarBrand>
@@ -71,7 +71,7 @@ const Header = (props) => {
 
                     <Nav className="mr-auto" navbar>
 
-                        <Form className="ml-md-5 py-2">
+                        <Form className="ml-md-0 ml-lg-5 py-2">
                             <FormGroup className="my-auto">
                                 <Input type="search" name="search" placeholder="Search ..." bsSize="sm" className="py-0" style={{ width: "250px" }} />
                             </FormGroup>
--- client/src/components/auth/Logout.js
+++ client/src/components/auth/Logout.js
@@ -20,7 +20,7 @@ const Logout = ({logout}) => {
 
   return (
     <>
-      <NavLink onClick={logingout} href="#"  className="m-0 p-0 px-xl-3">
+      <NavLink onClick={logingout} href="#"  className="m-0 p-0 px-sm-3">
         <Button color="warning" size="sm" className="py-0 px-1">
         <img src="https://img.icons8.com/color/18/000000/power-off-button.png" alt="logout"/>
         </Button>
