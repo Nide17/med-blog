d4553229476b042cb03c6b575b89e380d7f73312
Nide17 <nidedrogba@gmail.com>
2021-03-27 15:13:01 +0200

    ipad header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -52,12 +52,12 @@ const Header = (props) => {
     return (
         <header style={{ boxShadow: "0 2px 10px -1px rgba(0,0,0,0.75)" }}>
 
-            <Navbar color="primary" light expand="md" className="px-md-1 px-lg-5 py-md-3">
+            <Navbar color="primary" light expand="lg" className="px-lg-5 py-md-3">
                 <NavbarBrand href="/" className="text-white" style={{ fontWeight: "900" }}>
                     <img src={logo} alt="logo" />
                 </NavbarBrand>
 
-                <NavbarText className="mr-4 d-md-none">
+                <NavbarText className="mr-4 d-lg-none">
                     {blinkBtn}
                 </NavbarText>
 
@@ -71,20 +71,20 @@ const Header = (props) => {
 
                     <Nav className="mr-auto" navbar>
 
-                        <Form className="ml-md-0 ml-lg-5 py-2">
+                        <Form className="ml-lg-5 py-2">
                             <FormGroup className="my-auto">
                                 <Input type="search" name="search" placeholder="Search ..." bsSize="sm" className="py-0" style={{ width: "250px" }} />
                             </FormGroup>
                         </Form>
                     </Nav>
 
-                    <NavbarText className="mr-4 d-none d-sm-flex">
+                    <NavbarText className="mr-3 d-none d-lg-flex">
                         {blinkBtn}
                     </NavbarText>
-                    <NavbarText className="mr-4">
+                    <NavbarText className="mr-3">
                         <Link to="/about" className="text-white">About</Link>
                     </NavbarText>
-                    <NavbarText className="mr-4">
+                    <NavbarText className="mr-3">
                         <Link to="/contact" className="text-white">Contact</Link>
                     </NavbarText>
                     {isAuthenticated ? authLinks : guestLinks}
