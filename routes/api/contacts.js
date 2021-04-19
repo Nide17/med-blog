b0d1cf2f50855dfe67bc29207bedba61c169f996
Nide17 <nidedrogba@gmail.com>
2021-04-19 16:41:38 +0200

    contacts endpoints

---
+++ routes/api/contacts.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+
+// auth middleware to protect routes
+const { auth, authRole } = require('../../middleware/auth');
+
+//Contact Model : use capital letters since it's a model
+const Contact = require('../../models/Contact');
+
+// @route GET api/contacts
+// @route Get All contacts
+// @route Private: accessed by logged in user
+
+//we use router. instead of app. and / because we are already in this dir
+router.get('/', auth, async (req, res) => {
+
+  try {
+    const contacts = await Contact.find()
+      //sort contacts by creation_date
+      .sort({ test_date: -1 })
+      .populate('quiz')
+      .populate('taken_by')
+
+    if (!contacts) throw Error('No contacts found');
+
+    res.status(200).json(contacts);
+
+  } catch (err) {
+    res.status(400).json({ msg: err.message })
+  }
+});
+
+// @route POST api/contacts
+// @route Create a Contact
+// @route Private: accessed by logged in user
+
+router.post("/", async (req, res) => {
+
+  try {
+
+    const newContact = await Contact.create(req.body);
+    res.send(newContact);
+
+  } catch (err) {
+    console.log(err);
+
+    if (err.name === "ValidationError") {
+      return res.status(400).send(err.errors);
+    }
+    res.status(500).send("Something went wrong");
+  }
+});
+
+// @route GET api/contacts/:id
+// @route GET one Contact
+// @route Private: accessed by logged in user
+
+//:id placeholder, findId=we get it from the parameter in url
+router.get('/:id', auth, (req, res) => {
+
+  //Find the Contact by id
+  Contact.findById(req.params.id)
+
+    //return a promise
+    .then(contact => res.json(contact))
+    // if id not exist or if error
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+
+// @route DELETE api/contacts
+// @route delete a Contact
+// @route Private: Accessed by admin only
+
+//:id placeholder, findId=we get it from the parameter in url
+router.delete('/:id', auth, authRole(['Admin']), (req, res) => {
+
+  //Find the Contact to delete by id first
+  Contact.findById(req.params.id)
+
+    //returns promise 
+    .then(contact => contact.remove().then(() => res.json({ success: true })))
+    // if id not exist or if error
+    .catch(err => res.status(404).json({ success: false }));
+});
+
+module.exports = router;
