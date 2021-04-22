858c70c58dce3106e2700a79e44f92a951b6ea6d
Nide17 <nidedrogba@gmail.com>
2021-04-22 20:14:42 +0200

    Breadcrumb on editqtn

--- client/src/components/questions/EditQuestion.js
+++ client/src/components/questions/EditQuestion.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom'
+import { useParams, Link, useHistory } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
-import { Button, Col, Form, FormGroup, Label, Input, CustomInput } from 'reactstrap';
+import { Button, Row, Col, Form, FormGroup, Label, Input, CustomInput, Breadcrumb, BreadcrumbItem, Alert } from 'reactstrap';
 import { connect } from 'react-redux';
 import { setQuestions, updateQuestion } from '../../redux/questions/questions.actions';
 
@@ -27,7 +27,7 @@ const EditQuestion = ({ auth, updateQuestion, questionsData, setQuestions, allQu
     }, [setQuestions]);
 
     // Errors state on form
-    // const [errorsState, setErrorsState] = useState([])
+    const [errorsState, setErrorsState] = useState([])
 
     const onQuestionChangeHandler = e => {
         const { name, value } = e.target
@@ -53,19 +53,19 @@ const EditQuestion = ({ auth, updateQuestion, questionsData, setQuestions, allQu
         e.preventDefault();
 
         // VALIDATE
-        // if (questionTextState.questionText.length < 4) {
-        //     setErrorsState(['Insufficient info!']);
-        //     return
-        // }
-        // else if (questionTextState.questionText.length > 300) {
-        //     setErrorsState(['Question is too long!']);
-        //     return
-        // }
+        if (questionTextState.questionText.length < 4) {
+            setErrorsState(['Insufficient info!']);
+            return
+        }
+        else if (questionTextState.questionText.length > 300) {
+            setErrorsState(['Question is too long!']);
+            return
+        }
 
-        // else if (answerOptionsState.length <= 1) {
-        //     alert('Answers are not sufficient!')
-        //     return
-        // }
+        else if (answerOptionsState.length <= 1) {
+            alert('Answers are not sufficient!')
+            return
+        }
 
         const updatedQuestion = {
             qtId: questionId,
@@ -89,75 +89,77 @@ const EditQuestion = ({ auth, updateQuestion, questionsData, setQuestions, allQu
     }
 
     return (
-        // auth.isAuthenticated ?
-        <Form className="mt-5 mx-5" onSubmit={handleSubmit}>
+        auth.isAuthenticated ?
 
-            {/* {
-                    allQuizes && allQuizes.map(quiz =>
-                        quiz._id === questionId ?
-                            (<Row key={quiz._id} className="mb-3">
-                                <Breadcrumb key={quiz._id}>
-                                    <BreadcrumbItem><Link to="/webmaster">{quiz.category.title}</Link></BreadcrumbItem>
-                                    <BreadcrumbItem><Link to={`/category/${quiz.category._id}`}>{quiz.title}</Link></BreadcrumbItem>
-                                    <BreadcrumbItem active>Create Question</BreadcrumbItem>
-                                </Breadcrumb>
-                            </Row>) : ''
-                    )
-                } */}
+            <Form className="mt-5 mx-5" onSubmit={handleSubmit}>
 
-            {/* Error */}
-            {/* {errorsState.length > 0 ?
+                <Row className="mb-3">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to={`/category/:${selectedQuestion.category && selectedQuestion.category._id}`}>{selectedQuestion.category && selectedQuestion.category.title}</Link>
+                        </BreadcrumbItem>
+
+                        <BreadcrumbItem>
+                            <Link to={`/view-quiz/:${selectedQuestion.quiz && selectedQuestion.quiz._id}`}>{selectedQuestion.quiz && selectedQuestion.quiz.title}</Link>
+                        </BreadcrumbItem>
+
+                        <BreadcrumbItem active>Edit Question</BreadcrumbItem>
+                    </Breadcrumb>
+                </Row>
+
+                {/* Error */}
+                {errorsState.length > 0 ?
                     errorsState.map(err =>
                         <Alert color="danger" key={Math.floor(Math.random() * 1000)}>
                             {err}
                         </Alert>) :
                     null
-                } */}
+                }
 
-            <FormGroup row>
-                <Label for="examplequestion" sm={2}>Question Edit</Label>
-                <Col sm={10}>
-                    <Input type="text" name="questionText" value={questionTextState.questionText} id="examplequestion" placeholder="Question here ..." onChange={onQuestionChangeHandler} required />
-                </Col>
-            </FormGroup>
+                <FormGroup row>
+                    <Label for="examplequestion" sm={2}>Question Edit</Label>
+                    <Col sm={10}>
+                        <Input type="text" name="questionText" value={questionTextState.questionText} id="examplequestion" placeholder="Question here ..." onChange={onQuestionChangeHandler} required />
+                    </Col>
+                </FormGroup>
 
-            {answerOptionsState && answerOptionsState.map(answerOption => (
+                {answerOptionsState && answerOptionsState.map(answerOption => (
 
-                <div key={answerOption._id}>
+                    <div key={answerOption._id}>
 
-                    <FormGroup row>
-                        <Label for="exampleanswer" sm={2}>Answer</Label>
+                        <FormGroup row>
+                            <Label for="exampleanswer" sm={2}>Answer</Label>
 
-                        <Col sm={10} xl={7}>
-                            <Input type="text" name="answerText" value={answerOption.answerText}
-                                onChange={event => handleAnswerChangeInput(answerOption._id, event)} id="exampleanswer" placeholder="Answer here ..." required />
-                        </Col>
+                            <Col sm={10} xl={7}>
+                                <Input type="text" name="answerText" value={answerOption.answerText}
+                                    onChange={event => handleAnswerChangeInput(answerOption._id, event)} id="exampleanswer" placeholder="Answer here ..." required />
+                            </Col>
 
-                        <Col sm={6} xl={2} className="my-3 my-sm-2 d-sm-flex justify-content-around">
-                            <CustomInput type="checkbox" name="isCorrect" value={answerOption.isCorrect}
-                                onChange={event => handleAnswerChangeInput(answerOption._id, event)} id={answerOption._id} label="Is Correct?" required checked={answerOption.isCorrect} />
-                        </Col>
+                            <Col sm={6} xl={2} className="my-3 my-sm-2 d-sm-flex justify-content-around">
+                                <CustomInput type="checkbox" name="isCorrect" value={answerOption.isCorrect}
+                                    onChange={event => handleAnswerChangeInput(answerOption._id, event)} id={answerOption._id} label="Is Correct?" required checked={answerOption.isCorrect} />
+                            </Col>
 
-                        <Col sm={6} xl={1} className="my-3 my-sm-2">
-                            <Button disabled={answerOptionsState.length === 1} color="danger" onClick={() => handleRemoveFields(answerOption._id)}> - </Button>{' '}
-                            <Button color="danger" onClick={handleAddFields}> + </Button>{' '}
-                        </Col>
+                            <Col sm={6} xl={1} className="my-3 my-sm-2">
+                                <Button disabled={answerOptionsState.length === 1} color="danger" onClick={() => handleRemoveFields(answerOption._id)}> - </Button>{' '}
+                                <Button color="danger" onClick={handleAddFields}> + </Button>{' '}
+                            </Col>
 
-                    </FormGroup>
+                        </FormGroup>
 
-                </div>
+                    </div>
 
-            ))}
+                ))}
 
-            <FormGroup check row>
-                <Col sm={{ size: 10, offset: 2 }} className="pl-0">
-                    <Button className="btn btn-info btn-sm" type="submit" onClick={handleSubmit}>Add New</Button>
-                </Col>
-            </FormGroup>
+                <FormGroup check row>
+                    <Col sm={{ size: 10, offset: 2 }} className="pl-0">
+                        <Button className="btn btn-info btn-sm" type="submit" onClick={handleSubmit}>Add New</Button>
+                    </Col>
+                </FormGroup>
 
-        </Form>
-        //  :
-        // <h3 className="m-5 p-5 d-flex justify-content-center align-items-center text-danger">Login Again</h3>
+            </Form>
+            :
+            <h3 className="m-5 p-5 d-flex justify-content-center align-items-center text-danger">Login Again</h3>
     )
 }
 
