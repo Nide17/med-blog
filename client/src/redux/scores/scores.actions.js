970396ab202f5829da3dfa13c61c2ed0e1480b5b
Nide17 <nidedrogba@gmail.com>
2021-04-30 07:59:06 +0200

    removed alert for score saved

--- client/src/redux/scores/scores.actions.js
+++ client/src/redux/scores/scores.actions.js
@@ -31,9 +31,7 @@ export const createScore = (newScore) => async (dispatch, getState) => {
         dispatch({
           type: CREATE_SCORE,
           payload: res.data
-        }),
-        alert('Score Saved!')
-      )
+        }))
   } catch (err) {
     dispatch(returnErrors(err.response.data, err.response.status, 'CREATE_SCORE_FAIL'));
     dispatch({ type: CREATE_SCORE_FAIL })
