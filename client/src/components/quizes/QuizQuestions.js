352f35034228d314334f77584db69299f4c8f3a8
Nide17 <nidedrogba@gmail.com>
2021-05-15 13:23:37 +0200

    quiz questions feedback

--- client/src/components/quizes/QuizQuestions.js
+++ client/src/components/quizes/QuizQuestions.js
@@ -91,8 +91,8 @@ const QuizQuestions = ({ quizes, setQuizes, setQuestions, createScore, auth }) =
                                             
                                             {Math.round(score * 100 / quiz.questions.length) < 50 ?
                                                     <>
-                                                        <h4 className="text-center text-danger my-5">
-                                                You failed! please contact us for important books, guidance to review.
+                                                        <h4 className="text-center text-danger my-3">
+                                                You failed! you need more reading and practice to succeed. Please contact us for more important books, guidance that may help you.
                                                 </h4>
 
                                                         <Link to="/contact" className="text-success">
@@ -102,7 +102,7 @@ const QuizQuestions = ({ quizes, setQuizes, setQuestions, createScore, auth }) =
                                                 </Link>
                                                     </>
                                                 :
-                                                    <h4 className="text-center text-success my-5">
+                                                    <h4 className="text-center text-success my-3">
                                                 Congratulations, you passed this test!
                                                 </h4>}
                                             </div>
@@ -132,19 +132,19 @@ const QuizQuestions = ({ quizes, setQuizes, setQuestions, createScore, auth }) =
 
                                                 {Math.round(score * 100 / quiz.questions.length) < 50 ?
                                                     <>
-                                                        <h4 className="text-center text-danger my-5">
-                                                            You failed! please contact us for important books, guidance to review.
+                                                        <h4 className="text-center text-danger my-3">
+                                                            You failed! you need more reading and practice to succeed. Please contact us for more important books, guidance that may help you.
                                                         </h4>
 
                                                         <Link to="/contact" className="text-success">
                                                             <button type="button" className="btn btn-outline-success">
-                                                                Contact us for more
+                                                                Contact us for help!
                                                             </button>
                                                         </Link>
                                                     </>
                                                     :
-                                                    <h4 className="text-center text-success my-5">
-                                                        Congratulations, you passed this test!
+                                                    <h4 className="text-center text-success my-3">
+                                                        Congratulations, you passed this test! Remember, the more you practice the more you understand! 
                                                 </h4>}
                                             </div>
 
