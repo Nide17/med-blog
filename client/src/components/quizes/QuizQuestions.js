ecefdcce1905df01db5529ceb53caa62c045049c
Nide17 <nidedrogba@gmail.com>
2021-06-24 17:43:30 +0200

    contact us on passing the quiz

--- client/src/components/quizes/QuizQuestions.js
+++ client/src/components/quizes/QuizQuestions.js
@@ -43,8 +43,8 @@ const QuizQuestions = ({ quizes, setQuizes, createScore, auth }) => {
 
                     {
                         (nextQuestion < quiz.questions.length) ?
-                        setCurrentQuestion(nextQuestion) :
-                        setShowScore(true)
+                            setCurrentQuestion(nextQuestion) :
+                            setShowScore(true)
                     }
 
                     {quiz.questions[currentQuestion].answerOptions
@@ -126,7 +126,7 @@ const QuizQuestions = ({ quizes, setQuizes, createScore, auth }) => {
                                                     :
                                                     <h5 className="text-center text-success my-3">
                                                         Congratulations, you passed this test!
-                                                </h5>}
+                                                    </h5>}
                                             </div>
 
                                         </div> :
@@ -142,7 +142,7 @@ const QuizQuestions = ({ quizes, setQuizes, createScore, auth }) => {
                                             <a href={`/view-quiz/${quiz._id}`}>
                                                 <button type="button" className="btn btn-outline-success mt-3 mr-2 mr-md-3" onClick={Reload}>
                                                     Retake
-                                            </button>
+                                                </button>
                                             </a>
 
                                             <button type="button" className="btn btn-outline-success mt-3 p-0">
@@ -154,9 +154,9 @@ const QuizQuestions = ({ quizes, setQuizes, createScore, auth }) => {
                                                 {Math.round(score * 100 / quiz.questions.length) < 50 ?
 
                                                     <>
-                                                        <h5 className="text-center text-danger my-3">
+                                                        <h6 className="text-center text-danger my-3">
                                                             You failed! you need more reading and practice to succeed. Please contact us for more important books, guidance that may help you.
-                                                        </h5>
+                                                        </h6>
 
                                                         <Link to="/contact" className="text-success">
                                                             <button type="button" className="btn btn-outline-success">
@@ -164,10 +164,16 @@ const QuizQuestions = ({ quizes, setQuizes, createScore, auth }) => {
                                                             </button>
                                                         </Link>
                                                     </> :
-
-                                                    <h5 className="text-center text-success my-3">
-                                                        Congratulations, you passed this test! Remember, the more you practice the more you understand!
-                                                </h5>}
+                                                    <>
+                                                        <h6 className="text-center text-success my-3">
+                                                            Congratulations, you passed this test! Remember, the more you practice the more you understand! If you need any related book or help, don't hesitate to contact us!
+                                                        </h6>
+                                                        <Link to="/contact" className="text-success">
+                                                            <button type="button" className="btn btn-outline-success">
+                                                                Contact us for help!
+                                                            </button>
+                                                        </Link>
+                                                    </>}
                                             </div>
 
                                         </div> :
