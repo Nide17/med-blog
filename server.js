8ad4d22474002d4c16e7d55427af0e9857883f91
Nide17 <nidedrogba@gmail.com>
2021-07-12 00:44:44 +0200

    changed config vars hide

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 //DB Config
-const dbURI = config.get('mongoURI');
+const dbURI = process.env.MONGO_URI || config.get('mongoURI');
 
 //connect to Mongo
 const connectDB = async () => {
