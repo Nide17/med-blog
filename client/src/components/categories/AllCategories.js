30b2f477430793297278a82ac6fb99eb844f6fce
Nide17 <nidedrogba@gmail.com>
2021-07-14 20:05:08 +0200

    all cats height

--- client/src/components/categories/AllCategories.js
+++ client/src/components/categories/AllCategories.js
@@ -18,96 +18,99 @@ const AllCategories = ({ categories, setCategories, quizes, setQuizes }) => {
         setQuizes();
     }, [setCategories, setQuizes]);
 
-        return (
-            <TabPane tabId="4">
-                {categories.isLoading ?
-                    <ReactLoading type="spinningBubbles" color="#33FFFC" /> :
+    return (
+        <TabPane tabId="7">
+            {categories.isLoading ?
 
-                    <>
-                        <Row className="mt-3">
-                            {!quizes.isLoading ?
-                                <Col sm="6">
-                                    <SearchInput setSearchKey={setSearchKeyQ} placeholder=" Search quizes here ...  " />
-                                </Col> : null}
+                <div className="d-flex justify-content-center align-items-center" style={{ height: "80vh" }}>
+                    <ReactLoading type="spinningBubbles" color="#33FFFC" />
+                </div> :
 
+                <>
+                    <Row className="mt-3">
+                        {!quizes.isLoading ?
                             <Col sm="6">
-                                <SearchInput setSearchKey={setSearchKeyC} placeholder=" Search categories here ...  " />
-                            </Col>
-                        </Row>
+                                <SearchInput setSearchKey={setSearchKeyQ} placeholder=" Search quizes here ...  " />
+                            </Col> : null}
 
-                        {/* Search quizes */}
-                        <Row>
-                            <ListGroup>
-                                {quizes && quizes.allQuizes
-                                    .filter(quiz => {
+                        <Col sm="6">
+                            <SearchInput setSearchKey={setSearchKeyC} placeholder=" Search categories here ...  " />
+                        </Col>
+                    </Row>
 
-                                        if (searchKeyQ === "") {
-                                            return null
-                                        } else if (quiz.title.toLowerCase().includes(searchKeyQ.toLowerCase())) {
-                                            return quiz
-                                        }
+                    {/* Search quizes */}
+                    <Row>
+                        <ListGroup>
+                            {quizes && quizes.allQuizes
+                                .filter(quiz => {
+
+                                    if (searchKeyQ === "") {
                                         return null
-                                    })
-                                    .map(quiz => (
-                                        <ListGroupItem key={quiz._id} tag="a" href={`/view-quiz/${quiz._id}`}>
-                                            {quiz.title}
-                                        </ListGroupItem>
-                                    ))}
-                            </ListGroup>
-                        </Row>
+                                    } else if (quiz.title.toLowerCase().includes(searchKeyQ.toLowerCase())) {
+                                        return quiz
+                                    }
+                                    return null
+                                })
+                                .map(quiz => (
+                                    <ListGroupItem key={quiz._id} tag="a" href={`/view-quiz/${quiz._id}`}>
+                                        {quiz.title}
+                                    </ListGroupItem>
+                                ))}
+                        </ListGroup>
+                    </Row>
 
-                        {/* Search categories*/}
-                        <Row className="px-lg-5">
+                    {/* Search categories*/}
+                    <Row className="px-lg-5 pb-lg-5">
 
-                            {categories && categories.allcategories.filter(quiz => {
+                        {categories && categories.allcategories.filter(quiz => {
 
-                                if (searchKeyC === "") {
-                                    return quiz
-                                } else if (quiz.title.toLowerCase().includes(searchKeyC.toLowerCase())) {
-                                    return quiz
-                                }
-                                return null
-                            }).map(category => (
-                                <Col sm="6" key={category._id} className="mt-3 categories-toast">
+                            if (searchKeyC === "") {
+                                return quiz
+                            } else if (quiz.title.toLowerCase().includes(searchKeyC.toLowerCase())) {
+                                return quiz
+                            }
+                            return null
+                        }).map(category => (
+                            <Col sm="6" key={category._id} className="mt-3 categories-toast">
 
-                                    <Toast>
-                                        <ToastHeader className="text-success overflow-auto">
-                                            <strong>{category.title}</strong>
-                                        </ToastHeader>
+                                <Toast>
+                                    <ToastHeader className="text-success overflow-auto">
+                                        <strong>{category.title}</strong>
+                                    </ToastHeader>
 
-                                        <ToastBody>
-                                            <p className="font-weight-bold mb-0">
-                                                This category contains <span className="text-danger">{category.quizes.length}</span> quizzes</p>
-                                            <small className="text-center text-info">
-                                                <i>{category.description}</i>
-                                            </small>
+                                    <ToastBody>
+                                        <p className="font-weight-bold mb-0">
+                                            This category contains <span className="text-danger">{category.quizes.length}</span> quizzes</p>
+                                        <small className="text-center text-info">
+                                            <i>{category.description}</i>
+                                        </small>
 
-                                            <br />
-                                            <br />
-                                            {category.quizes && category.quizes.length > 0 ? <p className="font-weight-bold">Quizzes ({category.quizes.length})</p> : null}
+                                        <br />
+                                        <br />
+                                        {category.quizes && category.quizes.length > 0 ? <p className="font-weight-bold">Quizzes ({category.quizes.length})</p> : null}
 
-                                            {category && category.quizes.map((quiz, index) =>
-                                                <ul key={quiz._id} className="pl-1">
-                                                    <li style={{ listStyle: "none" }}>
-                                                        {index + 1}.&nbsp;
+                                        {category && category.quizes.map((quiz, index) =>
+                                            <ul key={quiz._id} className="pl-1">
+                                                <li style={{ listStyle: "none" }}>
+                                                    {index + 1}.&nbsp;
 
-                                                        <Link to={`/view-quiz/${quiz._id}`}>
-                                                            {quiz.title}
-                                                        </Link>
-                                                        <strong className="text-danger">&nbsp;
-                                                            ({quiz.questions.length} questions)</strong>
-                                                    </li>
-                                                </ul>
-                                            )}
-                                        </ToastBody>
-                                    </Toast>
+                                                    <Link to={`/view-quiz/${quiz._id}`}>
+                                                        {quiz.title}
+                                                    </Link>
+                                                    <strong className="text-danger">&nbsp;
+                                                        ({quiz.questions.length} questions)</strong>
+                                                </li>
+                                            </ul>
+                                        )}
+                                    </ToastBody>
+                                </Toast>
 
-                                </Col>
-                            ))}
-                        </Row></>}
-            </TabPane>
-        )
-    }
+                            </Col>
+                        ))}
+                    </Row></>}
+        </TabPane>
+    )
+}
 
 const mapStateToProps = state => ({
     categories: state.categoriesReducer,
